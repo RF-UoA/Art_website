ac9b6585e797348cda043f460eb69bb023d2ee71
RF-UoA <al884959@adelaide.edu.au>
2024-07-03 13:03:59 +0930

    Templates for all pages created

--- app.py
+++ app.py
@@ -4,7 +4,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('available-works.html', title='Available Works')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', title='Posts')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html', title='Portfolio')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        # Process the contact form submission here
+        pass
+    return render_template('contact.html', title='Contact')
 
 if __name__ == '__main__':
     app.run(debug=True)

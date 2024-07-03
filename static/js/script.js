49f3eb19f991f3844be4635bd4cbfbc2c058b20d
RF-UoA <al884959@adelaide.edu.au>
2024-07-03 19:26:37 +0930

    Snazzy side-menu created

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const burger = document.querySelector('.burger-menu');
+    const icon = burger.querySelector('i');
     const nav = document.querySelector('nav.strip.bottom-strip ul');
+    const bottomStrip = document.querySelector('nav.bottom-strip');
 
     burger.addEventListener('click', function() {
         nav.classList.toggle('active');
+        icon.classList.toggle('rotate');
+        bottomStrip.classList.toggle('active');
     });
 });
